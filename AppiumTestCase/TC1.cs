cecf787137a48dfe948cd8106ab87e1543397c2a
onurkahyaoglu <onurkahyaoglu@gmail.com>
2023-04-11 09:59:56 +0300

    emulatorü kod içerisinden başlatmak için gerekli düzenlemeler yapıldı

--- AppiumTestCase/TC1.cs
+++ AppiumTestCase/TC1.cs
@@ -27,8 +27,26 @@ namespace AppiumTestCase
             capabilities.SetCapability("appPackage", "com.android.chrome");
             capabilities.SetCapability("appActivity", "com.google.android.apps.chrome.Main");
 
+            //Bu kısım emulator'ü kod üzerinden çalıştırmak için
+            //ancak eğer bilgisayar çok hızlı değilse
+            //time out hatası alınıyor. Bu yüzden emülator'ü manuel başlatarak da çalıştırabiliriz.
+
+            capabilities.SetCapability("noReset", true);
+            capabilities.SetCapability("automationName", "UiAutomator2");
+            capabilities.SetCapability("avd", "Pixel_4_API_30");
+            capabilities.SetCapability("avdLaunchTimeout", 1200000);
+            capabilities.SetCapability("avdReadyTimeout", 1200000);
+            capabilities.SetCapability("avdArgs", "-no-snapshot-load");
+            capabilities.SetCapability("avdForceColdBoot", true); 
+            capabilities.SetCapability("newCommandTimeout", 300000);
+            capabilities.SetCapability("appWaitDuration", 300000);
+            capabilities.SetCapability("testdroid_testTimeout", 12000);
+
             Uri serverUri = new Uri("http://127.0.0.1:4723/wd/hub");
             driver = new AndroidDriver<AndroidElement>(serverUri, capabilities);
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(300);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(300);
         }
 
         [Test]
