5e1a8310363201011498196598fbd3669254e137
onurkahyaoglu <onurkahyaoglu@gmail.com>
2023-04-11 09:47:44 +0300

    SetUp oluşturuldu

--- AppiumTestCase/TC1.cs
+++ AppiumTestCase/TC1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,24 @@ namespace AppiumTestCase
 {
     public class TC1
     {
+        AndroidDriver<AndroidElement> driver;
+        DesiredCapabilities capabilities;
+
+        [SetUp]
+        public void InitDriver()
+        {
+            // Commandline da appium -a 127.0.0.1 -p 4723 komutu çalıştırılır.
+            // Bu sayede server ayağa kaldırılmış olur
+            // Sonrasında test yapılabilir
+
+            capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("platformName", "Android");
+            capabilities.SetCapability("deviceName", "Pixel_4_API_30");
+            capabilities.SetCapability("appPackage", "com.android.chrome");
+            capabilities.SetCapability("appActivity", "com.google.android.apps.chrome.Main");
+
+            Uri serverUri = new Uri("http://127.0.0.1:4723/wd/hub");
+            driver = new AndroidDriver<AndroidElement>(serverUri, capabilities);
+        }
     }
 }
